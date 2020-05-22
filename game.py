97ef321680cddb287ae241dec41e1831837b2ffb
Nazar <rohovsky@ucu.edu.ua>
2020-05-22 23:57:04 +0300

    Final commit

--- game.py
+++ game.py
@@ -1,20 +1,8 @@
-"""
-game = Game()
-game.play()
-    create board
-    while true:
-        person move
-        chevk win:
-            break
-        computer tree
-        computer move
-        check win:
-            break
-"""
 from board import Board
 from tree_analysis import gen_tree, count_wins
 from btree import LinkedBT
 from btnode import BTNode
+
 
 class Game:
     """
@@ -25,7 +13,7 @@ class Game:
         (Game) -> NoneType
         Creates a new object of Game class.
         """
-        self.board = Board([[0,0 ,0], [0,0,0], [0,0,0]])
+        self.board = Board([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
 
     def play(self):
         """
@@ -34,10 +22,12 @@ class Game:
         """
         print("""
     This is a game of tic-tac-toe, and you will be playing against computer.
-You will be shown a current board state and after that you will have to enter the postion
+You will be shown a current board state and after that you will have to \
+enter the postion
 where you want ot make move.
 
-    Your next move should be entered in this format: 0 2 , where 0 is a ROW and 2 is a COLUMN.
+    Your next move should be entered in this format: 0 2 , where 0 is a \
+ROW and 2 is a COLUMN.
 
 
 So now let us start, you will make the first move.""")
@@ -47,7 +37,8 @@ So now let us start, you will make the first move.""")
             move += 1
             print(self.board)
             right_input = False
-            pl_move = input('Please enter your move: ').replace(',', ' ').split(' ')
+            pl_move = input('Please enter your move: ').replace(',', ' ')
+            pl_move = pl_move.split(' ')
             while not right_input:
                 try:
                     post = (int(pl_move[0]), int(pl_move[1]))
@@ -55,7 +46,8 @@ So now let us start, you will make the first move.""")
                         raise Exception
                     right_input = True
                 except:
-                    pl_move = input('Please enter valid move: ').replace(',', ' ').split(' ')
+                    pl_move = input('Please enter valid move: ')
+                    pl_move = pl_move.replace(',', ' ').split(' ')
             self.board.board[post[0]][post[1]] = 'x'
             print(f'Board after {move} move:')
             print(self.board)
